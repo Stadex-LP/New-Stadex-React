788de51831bb65a57926d1cb6c3af78e5ef1b0ad
Dario-69 <57715903+Dario-69@users.noreply.github.com>
2023-04-07 23:55:58 +0200

    init project

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import './App.css';
+import DashboardLayout from './layouts/dashboard';
+import Manifestations from './views/manifestations';
+import Transports from './views/materiels';
+import Equipements from './views/equipements';
+import Organisateurs from './views/organisateurs';
+import Materiels from './views/materiels';
+import AddManifs from './views/manifestations/add';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<DashboardLayout/>} />
+        <Route path='/manifestations' element={<Manifestations/>} />
+        <Route path='/transports' element={<Transports/>} />
+        <Route path='/equipements' element={<Equipements/>} />
+        <Route path='/organisateurs' element={<Organisateurs/>} />
+        <Route path='/materiels' element={<Materiels/>} />
+
+        {/*  */}
+
+        <Route path='/manifestations/add' element={<AddManifs/>} />
+
+      </Routes>
+      
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/views/transports/index.js
@@ -0,0 +1,34 @@
+import DashboardLayout from "../../layouts/dashboard"
+import TransTable from "./components/transtable"
+
+function Transports() {
+  
+    return ( 
+      <DashboardLayout>
+        
+        <div class="h-screen px-4 pb-24 overflow-auto md:px-6">
+            
+
+            <div className="flex h-fit w-full items-center justify-between pt-4 pb-[20px] shadow-2xl shadow-gray-100 dark:!bg-navy-700 dark:shadow-none">
+                <div>
+                    <h1 class="text-4xl font-semibold text-gray-800 dark:text-white">
+                        Les Transports
+                    </h1>
+                    <h2 class="text-gray-400 text-md text-white">
+                        Ici retrouvrez la liste de tous les transports.
+                    </h2>
+                </div>
+
+                <button className="linear rounded-[20px] bg-lightPrimary px-4 py-2 text-base font-medium text-brand-500 transition duration-200 hover:bg-gray-100 active:bg-gray-200 dark:bg-white/5 dark:text-white dark:hover:bg-white/10 dark:active:bg-white/20">
+                    + Ajouter
+                </button>
+            </div>
+            <br/>
+            <TransTable/>
+        </div>
+
+      </DashboardLayout>
+    )
+  }
+  
+  export default Transports
