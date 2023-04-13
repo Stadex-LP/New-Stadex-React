721cbd147b2443102693455c4c10fd80b6e7f4aa
Dario-69 <57715903+Dario-69@users.noreply.github.com>
2023-04-14 01:35:40 +0200

    correction

--- src/views/manifestations/components/manifstable.jsx
+++ src/views/manifestations/components/manifstable.jsx
@@ -1,26 +1,30 @@
 import axios from "axios";
 import React from "react";
+import client from "../../../api";
 import Modal from "../../../components/modal";
 
-const client = axios.create({
-  baseURL: "http://localhost/api",
-  headers: {
-    "Content-Type": "application/json",
-    accept: "application/json",
-  },
-});
+
+const fetchData = async () => {
+  const response = await client.get('/manifestations');
+  const data = response.data;
+  return data;
+}
+
 
 function ManifTable() {
+
   const [showModal, setShowModal] = React.useState(false);
-  const [post, setPost] = React.useState(null);
+  const [post, setPost] = React.useState([]);
 
   React.useEffect(() => {
-    async function getPost() {
-      const response = await client.get("/manifestations/1");
-      console.log();
-      setPost(response.data);
+
+    const getData = async () => {
+      const response = await fetchData();
+      setPost(response);
     }
-    getPost();
+
+    getData();
+
   }, []);
 
   if (!post) return "Aucune manifestations";
@@ -44,31 +48,32 @@ function ManifTable() {
                   </tr>
                 </thead>
                 <tbody class="text-gray-600 text-sm font-light">
-                  <tr key={post.id}>
+                  {post.map(item => (
+                    <tr key={item.id}>
                     <td className="px-6 py-4 whitespace-nowrap">
-                      <div className="text-sm text-gray-900">{post.id}</div>
+                      <div className="text-sm text-gray-900">{item.id}</div>
                     </td>
 
                     <td className="px-6 py-4 whitespace-nowrap">
                       <div className="text-sm font-medium text-gray-900">
-                        {post.denomination}
+                        {item.denomination}
                       </div>
                     </td>
 
                     <td className="px-6 py-4 whitespace-nowrap text-left">
                       <div className="text-sm text-gray-500">
-                        {post.dateDebut}
+                        {item.dateDebut}
                       </div>
                     </td>
 
                     <td className="px-6 py-4 whitespace-nowrap text-left">
                       <div className="text-sm text-gray-500">
-                        {post.dateFin}
+                        {item.dateFin}
                       </div>
                     </td>
 
                     <td className="px-6 py-4 whitespace-nowrap text-left text-sm text-gray-500">
-                      {post.lieu}
+                      {item.lieu}
                     </td>
 
                     <td class="py-4 px-6 whitespace-nowrap text-left">
@@ -78,43 +83,27 @@ function ManifTable() {
                           onClick={() => setShowModal(true)}
                         >
                           <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                          >
-                            <path
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                              stroke-width="2"
-                              d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                            />
-                            <path
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                              stroke-width="2"
-                              d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-                            />
-                          </svg>
-                        </div>
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              stroke="currentColor"
+                            >
+                              <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                              />
+                              <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                              />
+                            </svg>
+                          </div>
                         </button>
-                        
-                        <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                          >
-                            <path
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                              stroke-width="2"
-                              d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
-                            />
-                          </svg>
-                        </div>
                         <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
                           <svg
                             xmlns="http://www.w3.org/2000/svg"
@@ -133,6 +122,8 @@ function ManifTable() {
                       </div>
                     </td>
                   </tr>
+                  ))}
+                  
                 </tbody>
               </table>
             </div>
@@ -149,7 +140,9 @@ function ManifTable() {
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/*header*/}
                 <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                  <h3 className="text-3xl font-semibold">Modal Title</h3>
+                  <h3 className="text-3xl font-semibold">
+                    Détails Manifestation
+                  </h3>
                   <button
                     className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                     onClick={() => setShowModal(false)}
@@ -162,11 +155,7 @@ function ManifTable() {
                 {/*body*/}
                 <div className="relative p-6 flex-auto">
                   <p className="my-4 text-slate-500 text-lg leading-relaxed">
-                    I always felt like I could do anything. That’s the main
-                    thing people are controlled by! Thoughts- their perception
-                    of themselves! They're slowed down by their perception of
-                    themselves. If you're taught you can’t do anything, you
-                    won’t do anything. I was taught I could do everything.
+                    INFORMATION
                   </p>
                 </div>
                 {/*footer*/}
@@ -177,13 +166,6 @@ function ManifTable() {
                     onClick={() => setShowModal(false)}
                   >
                     Close
-                  </button>
-                  <button
-                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setShowModal(false)}
-                  >
-                    Save Changes
                   </button>
                 </div>
               </div>
---
+++ src/views/transports/components/addform.js
@@ -0,0 +1,87 @@
+import axios from "axios";
+import React, {useState} from "react";
+import client from "../../../api";
+
+const postData = async (data) => {
+  const response = await client.post('/transports', data);
+  return response.data;
+}
+
+function TransForm() {
+
+  
+  const [Libelle, setlibelle] = useState("");
+  const [PrixHorraire, setprixho] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const data = { Libelle, PrixHorraire };
+
+    const response = await postData(data);
+    console.log(response);
+
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="space-y-12">
+        <div className="border-b border-gray-900/10 pb-12">
+          <div className="col-span-full">
+            <label
+              htmlFor="Libelle"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Transport
+            </label>
+            <div className="mt-2">
+              <input
+                type="text"
+                name="Libelle"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                value={Libelle}
+                onChange={(event) => setlibelle(event.target.value)}
+              />
+            </div>
+          </div>
+
+          <div className="mt-5 col-span-full">
+            <label
+              htmlFor="PrixHorraire"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Prix/Horraire transport
+            </label>{" "}
+            <div className="mt-2">
+              <input
+                type="num"
+                name="PrixHorraire"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                value={PrixHorraire}
+                onChange={(event) => setprixho(event.target.value)}
+              />{" "}
+              &euro;
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-6 flex items-center justify-end gap-x-6">
+        <button
+          type="button"
+          className="text-sm font-semibold leading-6 text-gray-900"
+        >
+          Cancel
+        </button>
+        <button
+          type="submit"
+          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Save
+        </button>
+      </div>
+    </form>
+  );
+}
+
+export default TransForm;
